408d4b96527b144d07448f915a0a7598eef534e7
Saroj Rai <raisaroj360@gmail.com>
2022-07-11 00:54:35 +0545

    added navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,57 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <div className="">
       <Head>
         <title> Futsal House </title>
-        <meta name="description" content="Futsal House, online Booking, Matching, Competing site for Futsal Players and Centres." />
+        <meta
+          name="description"
+          content="Futsal House, online Booking, Matching, Competing site for Futsal Players and Centres."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-red-400">
-        <h1 className="text-green-600">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className="">
+        <div className="relative">
+          <div className="w-full h-full absolute top-0 -z-10">
+            <Image src="/images/cover.png" width="1440px" height="660px" layout="responsive"/>
+          </div>
+          
+          <nav className="w-full flex justify-between items-center px-40 py-5">
+            <div>
+              <Image
+                src="/images/logo.png"
+                alt="Futsal House Logo"
+                width="114px"
+                height="44px"
+                layout="intrinsic"
+              />
+            </div>
+            <ul className="flex justify-between gap-8">
+              <li className="px-4 text-lg font-medium text-white border-b-2 border-b-transparent hover:border-b-white transition-all">
+                <a href="#"> Home </a>
+              </li>
+              <li className="px-4 text-lg font-medium text-white border-b-2 border-b-transparent hover:border-b-white transition-all">              
+                <a href="#"> Futsal </a>
+              </li>
+              <li className="px-4 text-lg font-medium text-white border-b-2 border-b-transparent hover:border-b-white transition-all">
+                <a href="#"> About </a>
+              </li>
+              <li className="px-4 text-lg font-medium text-white border-b-2 border-b-transparent hover:border-b-white transition-all">
+                <a href="#"> Contact </a>
+              </li>
+            </ul>
+          </nav>
 
+        </div>
       </main>
 
-      <footer className="">
-      </footer>
+      <footer className=""></footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

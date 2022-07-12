08d97ecd9d425365f97f7736664596bd11ea0990
Saroj Rai <raisaroj360@gmail.com>
2022-07-12 23:41:26 +0545

    hero ui update

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,20 +17,57 @@ const Home: NextPage = () => {
 
       <main className="">
         <div className="relative">
-          <Navbar/>
-          <div className="w-full h-full absolute top-0 -z-10">
-            <Image alt="Futsal House - Cover photo" src="/images/cover.png" width="1440px" height="660px" layout="responsive"/>
+          <Navbar />
+          <div className="w-full h-full absolute top-0 -z-10 backdrop-blur-lg">
+            <Image
+              alt="Futsal House - Cover photo"
+              src="/images/cover.png"
+              width="1440px"
+              height="660px"
+              layout="responsive"
+              quality={100}
+            />
           </div>
-	  <div className="relative flex justify-center items-center">
-	    <Image alt="Futsal House - Stadium Graphic" src="/images/hero_graphic.png" width={"770"} height={"580"} quality={100}/>
-	    <button className="absolute h-14 px-40 mt-40 bg-green-500 rounded-md shadow-2xl border-b-2 border-b-emerald-800 hover:bg-green-600 hover:scale-105 transition-all">
-	      <p className="font-bold text-3xl text-white"> BOOK NOW </p>
-            </button>
-          </div>
-	  <div className="flex">
+          <div className="relative flex justify-center items-center mt-10 ">
+            <Image
+              alt="Futsal House - Stadium Graphic"
+              src="/images/hero_graphic.png"
+              width={"350"}
+              height={"260"}
+              quality={100}
+            />
+            <div className="absolute -mb-96">
+              <div className="flex gap-5">
+                <div className="h-10 min-w-[34rem] bg-white rounded-3xl shadow-2xl">
+                  <input
+                    name=""
+                    type="text"
+                    value=""
+                    placeholder="Location"
+                    className="h-10 px-8 rounded-3xl shadow-2xl outline-none"
+                  />
+                  <input
+                    name=""
+                    type="date"
+                    value=""
+                    placeholder="Date"
+                    className="h-10 px-4 rounded-3xl shadow-2xl outline-none text-gray-500"
+                  />
+                  <input
+                    name=""
+                    type="time"
+                    value=""
+                    placeholder="Time"
+                    className="h-10 px-4 rounded-3xl shadow-2xl outline-none text-gray-500"
+                  />                                    
+                </div>
+                <button className=" h-10 px-20 bg-green-500 rounded-3xl shadow-2xl hover:bg-green-600 hover:scale-105 transition-all">
+                  <p className="font-bold text-xl text-white"> Search </p>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-        
       </main>
 
       <footer className=""></footer>
